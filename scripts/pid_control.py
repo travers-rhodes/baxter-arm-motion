32bad0a4148ae318f11306f9a45e6bcbfd8e84da
Travers Rhodes <traversr@andrew.cmu.edu>
2018-04-23 15:02:56 -0400

    I think this controller is doing the right thing :monkey: :see-no-evil:?

--- scripts/pid_control.py
+++ scripts/pid_control.py
@@ -1,31 +1,52 @@
 #!/usr/bin/python
 import rospy
 import numpy as np
+import ik_client
 
 from baxter_pykdl import baxter_kinematics
 from baxter_core_msgs.msg import JointCommand
+from sensor_msgs.msg import JointState
 
+joint_names = [ 'right_e0', 'right_e1', 'right_s0', 'right_s1', 'right_w0', 'right_w1', 'right_w2']
+
+class BaxterCache:
+    def __init__(self):
+      self.listener = rospy.Subscriber("/robot/joint_states", JointState, self.callback)
+      self.joints = None
+
+    def callback(self, mesg):
+      self.joints = np.array([dict(zip(mesg.name,mesg.position))[joint_name] for joint_name in joint_names])
 
 def main():
     rospy.init_node('pid_control')
+    bc = BaxterCache()
     controlTopic = "/robot/limb/right/joint_command"
     pub = rospy.Publisher(controlTopic, JointCommand)
     messageObj = JointCommand()
     messageObj.mode = JointCommand.TORQUE_MODE
-    messageObj.names = [ 'right_e0', 'right_e1', 'right_s0', 'right_s1', 'right_w0', 'right_w1', 'right_w2']
+    messageObj.names = joint_names
 
     kin = baxter_kinematics('right')
     kin.print_kdl_chain()
 
     # Desired end position (don't care about orientation to start)
-    pos = [0.582583, -0.180819, 0.216003]
-    desired_joints = np.array(kin.inverse_kinematics(pos))
+    pos = [0.582583, -0.80819, 0.036003]
+    ### TODO CHANGE TO SERVICE CALL
+    print("This breaks???")
+    desired_joints = np.array(ik_client.ik("right",pos))
 
-    rate = rospy.Rate(10) #10 Hz
+
+    rate = rospy.Rate(100) #10 Hz
+    print("Waiting for first joint_state message")
+    while bc.joints == None:
+      rate.sleep()
+    print("First joint_state message acquired")
     while not rospy.is_shutdown(): 
       # compute the current joint angles
-      cur_joints = np.array(kin.forward_position_kinematics())
+      cur_joints = bc.joints 
+      print(cur_joints, "Cur_joints")
       grad = desired_joints - cur_joints
+      print(grad, desired_joints)
       torques = compute_torque(grad)
       messageObj.command = [float(torque) for torque in torques.tolist()]
       pub.publish(messageObj)
@@ -36,7 +57,7 @@ def main():
 # return the desired joint torque to apply to each joint for a simple
 # proportional controller.
 def compute_torque(joint_grad):
-    torques = joint_grad * 10.0
+    torques = joint_grad * 100000#np.array([100,100000,1000,100000,1,1,1])
     return torques
       
       

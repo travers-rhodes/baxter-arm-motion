a3d9d3e712c0cc936db2ed8fa74fdaf0fc12316e
travers-rhodes <traversr@andrew.cmu.edu>
2018-04-25 13:06:34 -0400

    It seems to work now! Not quite sure what was wrong, actually. :sparkling_heart:

--- scripts/pid_control.py
+++ scripts/pid_control.py
@@ -4,9 +4,11 @@ import numpy as np
 import ik_client
 import time
 import csv
+import math
 
 from baxter_core_msgs.msg import JointCommand
 from sensor_msgs.msg import JointState
+import baxter_tools
 
 joint_names = [ 'right_e0', 'right_e1', 'right_s0', 'right_s1', 'right_w0', 'right_w1', 'right_w2']
 
@@ -40,62 +42,66 @@ class StateLogger:
     def log(self, data):
       self.writer.writerow(data)
 
-
-def main():
-    rospy.init_node('pid_control')
+def runExperiment(Kp, Kd, Ki, commandHz):
     bc = BaxterCache()
+
+    print("Resetting robot")
+    tucker = baxter_tools.Tuck(False)
+    rospy.on_shutdown(tucker.clean_shutdown)
+    tucker.supervised_tuck()
+    rospy.sleep(1)
+
     controlTopic = "/robot/limb/right/joint_command"
     pub = rospy.Publisher(controlTopic, JointCommand)
     messageObj = JointCommand()
     messageObj.mode = JointCommand.TORQUE_MODE
     messageObj.names = joint_names
 
+    rate = rospy.Rate(commandHz)
+
     # Desired end position (don't care about orientation to start)
-    #pos = [0.582583, -0.60819, 0.66003]
-    #desired_joints = np.array(ik_client.ik("right",pos))
     desired_joints = [1.27783743020085, 1.940743404428984, -1.4335451199369675, -0.3248315839206377, 2.0553848590877415, -1.0419260900570144, -2.649420877958835]
-
-    # Start out moving the robot arm using the native controller
-    messageObj.mode = JointCommand.POSITION_MODE
-    messageObj.command = [float(j) for j in desired_joints]
-    rate = rospy.Rate(1000) #10 Hz
-    print("Sending joint commands for 3 seconds...")
-    timeout = time.time() + 3 # timeout after 3 seconds 
-#    while time.time() < timeout: 
-#      pub.publish(messageObj)
-#      rate.sleep() 
-
 
     print("Waiting for first joint_state message")
     while bc.joints is None:
       rate.sleep()
     print("First joint_state message acquired")
     cumulativeJointError = np.zeros(bc.joints.shape)
-    with StateLogger("log.csv") as logger:
+    fileName = "results/logKp%fKd%fKi%fHz%d.csv" % (Kp, Kd, Ki, commandHz)
+    print("logging to %s" % fileName)
+    with StateLogger(fileName) as logger:
       it = 0
-      while not rospy.is_shutdown():
+      maxIt = 20*commandHz
+      while not rospy.is_shutdown() and it < maxIt:
         # compute the current joint angles
         cur_joints = bc.joints 
         grad = cur_joints - desired_joints 
         # note this needs to be scaled by freq of messages
-        cumulativeJointError += grad
+        cumulativeJointError += grad / commandHz
         grad_dot = bc.joints_dot
-        if it % 100 == 0:
+        # log at 10 hz
+        if it % int(max(1,math.floor(commandHz/10))) == 0:
           logger.log(grad.tolist() +  grad_dot.tolist() + cumulativeJointError.tolist())
+          #print(it, commandHz)
         it += 1
-        torques = compute_torque(grad, grad_dot, cumulativeJointError)
+        torques = compute_torque(grad, grad_dot, cumulativeJointError, Kp, Kd, Ki)
         messageObj.command = [float(torque) for torque in torques.tolist()]
         pub.publish(messageObj)
         rate.sleep()
+
+def main():
+    rospy.init_node('pid_control')
+    for commandHz in [10,100,1000]:
+      for Ki in [0.1, 10]:
+        for Kd in [0.1, 10]:
+          for Kp in [100, 10, 1]:
+            runExperiment(Kp, Kd, Ki, commandHz)
        
 
 # given joint_grad (the difference desired_joints minus current_joints)
 # return the desired joint torque to apply to each joint for a simple
 # proportional controller.
-def compute_torque(error, error_dot, error_sum):
-    Kp = 0.1
-    Kd = 1
-    Ki = 0.0001
+def compute_torque(error, error_dot, error_sum, Kp, Kd, Ki):
     torques = (- Kp * error - Kd * error_dot - Ki * error_sum)
     return torques
 

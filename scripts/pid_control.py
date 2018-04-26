567a4d4deb956eda4391c0307169b2c5819f6564
travers-rhodes <traversr@andrew.cmu.edu>
2018-04-26 19:33:19 -0400

    results for slides

--- scripts/pid_control.py
+++ scripts/pid_control.py
@@ -6,43 +6,100 @@ import time
 import csv
 import math
 
-from baxter_core_msgs.msg import JointCommand
+from baxter_core_msgs.msg import JointCommand, EndpointState
 from sensor_msgs.msg import JointState
 from baxter_arm_motion.msg import Tracking
+from visualization_msgs.msg import Marker
 import baxter_tools
 
 joint_names = [ 'right_e0', 'right_e1', 'right_s0', 'right_s1', 'right_w0', 'right_w1', 'right_w2']
+Kp = 7
+Kd = 0
+Ki = 5
 
 class BaxterCache:
     def __init__(self):
+      self.trackFuture = False#True#False
       self.listener = rospy.Subscriber("/robot/joint_states", JointState, self.curstate_callback)
       self.desListener = rospy.Subscriber("/follow/target_point", Tracking, self.desired_callback)
+      self.truthListener = rospy.Subscriber("/follow/truth", Marker, self.truth_callback)
+      self.endListener = rospy.Subscriber("/robot/limb/right/endpoint_state", EndpointState, self.end_effect_callback)
       self.joints = None
       self.joints_dot = None
-      self.desired_joints = None 
+      self.desired_pos = None 
+      self.desired_pos_next = None
+      self.desired_time = None
+      self.last_desired_joints = None
 
+    def truth_callback(self, mesg):
+      pos = mesg.points[0]
+      self.truth = [pos.x,pos.y,pos.z]
+
+    def end_effect_callback(self, mesg):
+      pos = mesg.pose.position
+      self.end_eff_state = [pos.x,pos.y,pos.z]
+    
     def curstate_callback(self, mesg):
       tmpdict = dict(zip(mesg.name,range(len(mesg.name))))
       self.joints = np.array([mesg.position[tmpdict[joint_name]] for joint_name in joint_names])
-      alpha = 0
-      if not self.joints_dot is None:
-        self.joints_dot = self.joints_dot * alpha + (1-alpha) * np.array([mesg.velocity[tmpdict[joint_name]] for joint_name in joint_names])
-      else:
-        self.joints_dot = np.array([mesg.velocity[tmpdict[joint_name]] for joint_name in joint_names])
+      self.joints_dot = np.array([mesg.velocity[tmpdict[joint_name]] for joint_name in joint_names])
 
     def desired_callback(self, mesg):
-      pos = [mesg.current.x, mesg.current.y, mesg.current.z]
+      self.desired_pos = np.array([mesg.current.x, mesg.current.y, mesg.current.z])
+      self.desired_pos_next = np.array([mesg.future.x, mesg.future.y, mesg.future.z])
+      self.desired_time = mesg.curTime
+
+    def get_interp_pos(self):
+      curtime = rospy.get_rostime()
+      # time stamp is 1 second off, so divide by 1 here ellipsized
+      pos = self.desired_pos + (curtime - self.desired_time).to_sec() * (self.desired_pos_next - self.desired_pos)
       print(pos)
+      print(self.desired_pos)
+      return pos
+
+    def get_desired_joints_and_vel(self):
+      if self.desired_pos is None:
+        return None
+      if self.trackFuture:
+        start_joints = self.get_ordered_joints(self.desired_pos)
+        if (start_joints is None and self.last_desired_joints is not None):
+          return (self.last_desired_joints, self.last_desired_dot)
+        elif (start_joints is None):
+          return None
+        future_joints = self.get_ordered_joints(self.desired_pos_next)
+        if (future_joints is None and self.last_desired_joints is not None):
+          return (self.last_desired_joints, self.last_desired_dot)
+        elif (future_joints is None):
+          return None
+        interp_pos = self.get_interp_pos()
+        #print(interp_pos)
+        desired_joints = self.get_ordered_joints(interp_pos)
+        # it turns out this linearization is not a good estimate
+        # of desired velocity, so instead we go with zero here
+        desired_dot = np.zeros(future_joints.shape) #- start_joints
+      else: 
+        desired_joints = self.get_ordered_joints(self.desired_pos)
+        if (desired_joints is None and self.last_desired_joints is not None):
+          return (self.last_desired_joints, self.last_desired_dot)
+        elif (desired_joints is None):
+          return None
+        desired_dot = np.zeros(desired_joints.shape) 
+      self.last_desired_joints = desired_joints
+      self.last_desired_dot = desired_dot
+      return (desired_joints, desired_dot)
+
+    def get_ordered_joints(self, pos):
       ik_resp = ik_client.ik("right",pos)
+      if ik_resp == 0:
+        return
       ik_resp_joints = ik_resp[0]
       ik_resp_names = ik_resp[1]
       tmpdict = dict(zip(ik_resp_names,range(len(ik_resp_names))))
-      self.desired_joints = [ik_resp_joints[tmpdict[nm]] for nm in joint_names]
-       
+      return np.array([ik_resp_joints[tmpdict[nm]] for nm in joint_names])
 
 class StateLogger:
     def __init__(self, logFileName):
-      self.mock = True
+      self.mock = False
       if not self.mock:
         self.logFile = open(logFileName, 'w')
         self.writer = csv.writer(self.logFile)
@@ -85,26 +142,31 @@ def runExperiment(Kp, Kd, Ki, commandHz):
     print("First joint_state message acquired")
     
     print("Waiting for first target_loc message")
-    while bc.desired_joints is None:
+    while bc.get_desired_joints_and_vel() is None:
       rate.sleep()
     print("First target_loc message acquired")
+    time.sleep(1)
     
     cumulativeJointError = np.zeros(bc.joints.shape)
-    fileName = "results/logKp%fKd%fKi%fHz%d.csv" % (Kp, Kd, Ki, commandHz)
+    fileName = "results/Results%s.csv" % ("Interp" if bc.trackFuture else "Naive")
     print("logging to %s" % fileName)
     with StateLogger(fileName) as logger:
       it = 0
       maxIt = 2000*commandHz
-      while not rospy.is_shutdown() and it < maxIt:
+      while not rospy.is_shutdown():# and it < maxIt:
+        des = bc.get_desired_joints_and_vel()
+        if des is None or des[0] is None:
+          continue
+        desired_joints = des[0]
+        desired_dot = des[1]
         # compute the current joint angles
-        err = bc.joints - bc.desired_joints 
+        err = bc.joints - desired_joints 
         # note this needs to be scaled by freq of messages
         cumulativeJointError += err / commandHz
-        err_dot = bc.joints_dot
-        # log at 10 hz
-        if it % int(max(1,math.floor(commandHz/10))) == 0:
-          logger.log(err.tolist() +  err_dot.tolist() + cumulativeJointError.tolist())
-          #print(it, commandHz)
+        err_dot = bc.joints_dot - desired_dot
+        # log at 40 hz
+        if it % int(max(1,math.floor(commandHz/40))) == 0:
+          logger.log([rospy.get_rostime()] + bc.end_eff_state + bc.truth)
         it += 1
         torques = compute_torque(err, err_dot, cumulativeJointError, Kp, Kd, Ki)
         messageObj.command = [float(torque) for torque in torques.tolist()]
@@ -113,18 +175,18 @@ def runExperiment(Kp, Kd, Ki, commandHz):
 
 def main():
     rospy.init_node('pid_control')
-    for commandHz in [100]:
-      for Ki in [0.1]:
-        for Kd in [1]:
-          for Kp in [10]:
-            runExperiment(Kp, Kd, Ki, commandHz)
+    commandHz = 1000
+    runExperiment(Kp, Kd, Ki, commandHz)
        
 
 # given joint_grad (the difference desired_joints minus current_joints)
 # return the desired joint torque to apply to each joint for a simple
 # proportional controller.
 def compute_torque(error, error_dot, error_sum, Kp, Kd, Ki):
+    # clip torques to avoid large torques
+    maxtorque = 30
     torques = (- Kp * error - Kd * error_dot - Ki * error_sum)
+    torques = np.array([min(max(t,-maxtorque),maxtorque) for t in torques])
     return torques
 
 if __name__ == "__main__":
--- scripts/target_generation.py
+++ scripts/target_generation.py
@@ -3,7 +3,9 @@ import rospy
 import numpy as np
 
 from baxter_arm_motion.msg import Tracking
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, Vector3
+from visualization_msgs.msg import Marker
+from std_msgs.msg import ColorRGBA
 
 ####
 ## This method generateMessage can be modified to fill in a real trajectory.
@@ -19,17 +21,39 @@ def generateMessage(secondsSinceStart):
   future = circle_path(secondsSinceStart + 1)
   msg.current = Point(current[0], current[1], current[2])
   msg.future = Point(future[0], future[1], future[2])
+  msg.curTime = rospy.get_rostime()
   return msg
 
+def generateFullTrajMsg():
+  mesg = Marker()
+  mesg.type = Marker.LINE_STRIP
+  mesg.id = 0;
+  mesg.scale.x = 0.05 # = Vector3(1.0, 0, 0)
+  mesg.color = ColorRGBA(0.0, 0.0, 1.0, 0.5)
+  mesg.points = [circle_path_point(t) for t in np.linspace(0,np.pi * 2)]
+  mesg.header.frame_id = 'torso'
+  return mesg 
+
+def generateTargetVisMsg(secondsSinceStart):
+  mesg = Marker()
+  mesg.type = Marker.POINTS
+  mesg.id = 0;
+  mesg.scale.x = 0.05 # = Vector3(1.0, 0, 0)
+  mesg.color = ColorRGBA(0.0, 0.0, 1.0, 0.5)
+  mesg.points = [circle_path_point(secondsSinceStart)]
+  mesg.header.frame_id = 'torso'
+  return mesg 
+   
+def circle_path_point(secondsSinceStart):
+  vec = circle_path(secondsSinceStart)
+  return Point(vec[0], vec[1], vec[2])
+
 def circle_path(secondsSinceStart):
-  center = np.array([0.656982770038,
-                    -0.852598021641,
-                    0.1388609422173])
   center = np.array([ 0.672040974663,
                      -0.724013073822,
                       0.0569658745728])
 
-  omg = 1
+  omg = 0.5
   radius = 0.1
   point = center + radius * np.array([0,np.sin(omg * secondsSinceStart), np.cos(omg * secondsSinceStart)])
   return point
@@ -37,15 +61,29 @@ def circle_path(secondsSinceStart):
 class TargetGeneration:
   def __init__(self):
     # publish at 100 Hz
-    self.freq = 100
+    self.freq = 2
     self.publisher = rospy.Publisher("/follow/target_point", Tracking, queue_size = 10)
+    self.trajpublisher = rospy.Publisher("/follow/full_traj", Marker, queue_size = 10)
+    self.targpublisher= rospy.Publisher("/follow/target_mark", Marker, queue_size = 10)
+    self.truthpublisher= rospy.Publisher("/follow/truth", Marker, queue_size = 10)
     rate = rospy.Rate(self.freq)
-    starttime = rospy.get_time()
+    self.starttime = rospy.get_time()
     print("sending targeting messages")
-    while not rospy.is_shutdown():
-      msg = generateMessage(rospy.get_time() - starttime)
+    rospy.Timer(rospy.Duration(1.0/self.freq), self.slowCallback)
+    rospy.Timer(rospy.Duration(1.0/200), self.fastCallback)
+
+  def slowCallback(self,event):
+      msg = generateMessage(rospy.get_time() - self.starttime)
       self.sendMessage(msg)
-      rate.sleep()
+      pathmsg = generateFullTrajMsg()
+      self.trajpublisher.publish(pathmsg)
+      targmsg = generateTargetVisMsg(rospy.get_time() - self.starttime)
+      self.targpublisher.publish(targmsg)
+
+  def fastCallback(self, event):
+      targmsg = generateTargetVisMsg(rospy.get_time() - self.starttime)
+      self.truthpublisher.publish(targmsg)
+        
 
   def sendMessage(self, msg):
     self.publisher.publish(msg) 
